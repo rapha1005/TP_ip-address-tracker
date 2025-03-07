0ef26571fa0d691b60ccbe7fcb954a706463f594
tdimnet <thomas.dimnet@gmail.com>
2025-03-07 11:31:16 +0100

    Ready to start to connect the other API to collect data

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # TP IP Address Tracker
 
-Lien vers l'API de géolocalisation : https://geo.ipify.org/
-
+- Lien vers l'API de géolocalisation : https://geo.ipify.org/
+- Lien vers l'API de LeafLet : https://leafletjs.com/examples/quick-start/
 
--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-console.log("===")
-console.log("===")
-console.log("===")
+const $ipAddressForm = document.querySelector(".ip-address-form")
 
 const map = L.map('map').setView([51.505, -0.09], 13);
 
@@ -8,3 +6,13 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
+
+
+$ipAddressForm.addEventListener("click", function(e) {
+    e.preventDefault()
+
+    const $ipAddressInput = document.querySelector(".ip-address-input")
+    const ipAddress = $ipAddressInput.value
+
+    console.log("The IP Address is ", ipAddress)
+})
